7a558e5cb4250a2fe77d661b90b8342a5a0893b6
sss654654 <zed6740@naver.com>
2022-06-28 14:14:08 +0900

    test commit(subin1)

---
+++ src/main/java/com/example/studyweb/StudywebApplication.java
@@ -0,0 +1,14 @@
+package com.example.studyweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudywebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudywebApplication.class, args);
+		// test
+	}
+
+}
